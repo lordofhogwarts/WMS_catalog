74dd9c1d6b19b78477b57f7019e6481283c2e7ac
Thao <cht081@k024-mp04.hmdc.harvard.edu>
2017-06-21 16:01:16 -0400

    second

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns=[
 	url(r'^$',views.index,name='index'),
 
-	url(r'^$',views.s_detail,name='detail'),
+	url(r'^(?P<service_id>[0-9]+)/$',views.s_detail,name='detail'),
 
-	url(r'^$',views.layer,name='layer')
+	url(r'^(?P<service_id>[0-9]+)/(?P<layer_id>[0-9]+)/$',views.layer,name='layer'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse			
-
+from django.http import HttpResponse
+from catalog.models import Service, Layer		
+from django.template import loader
 # Create your views here.
 def index(request):
-	service_name = Service.
-	return HttpResponse("You have reached the catalog")
+	service_list = Service.objects.all()
+#	template = loader.get_template('catalog/index.html')
+	context = {
+	 'service_list' : service_list,
+	}
+	return render(request,'catalog/index.html',context)
 
-def s_detail(request):
-	return HttpResponse("You are looking at the details of Service")
+def s_detail(request,service_id):
+	service = Service.objects.get(pk=service_id)
+	context= {'service':service}
+	return render(request,'catalog/detail.html',context)
 
-def layer(request):
-	return HttpResponse("Welcome to the layer index")
+def layer(request,service_id,layer_id):
+	layer = Layer.objects.get(pk=layer_id)
+	context= {'layer':layer}
+	return render(request,'catalog/layer.html',context)
 	
